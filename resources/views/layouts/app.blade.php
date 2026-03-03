3f3ce9e270fb339bb4f7ddced0559b413897ad18
maurocastillorondinara <nolosonie1@gmail.com>
2026-03-02 22:32:13 -0300

    refactor: Rebrand layout defaults to FAMDQ

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,9 +3,9 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="description" content="@yield('meta_description', 'Liga de Football Flag de Mar del Plata - Sin contacto, máxima intensidad. Unite a la revolución del deporte en la costa.')">
+    <meta name="description" content="@yield('meta_description', 'Fútbol Americano Mar del Plata - Trabajo en equipo, disciplina, máxima intensidad.')">
     <link rel="icon" type="image/png" href="{{ asset('images/logo-liga.png') }}">
-    <title>@yield('title', 'Liga de Flag Football MDP')</title>
+    <title>@yield('title', 'FAMDQ - Fútbol Americano Mar del Plata')</title>
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 <body>
